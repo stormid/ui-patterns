3844d4cc55bbc02aecb73039835e6464dc8d219d
Mick Perkins <mick.perkins@stormid.com>
2021-07-02 15:41:45 +0100

    Update example styles

--- src/templates/pages/example/modal-search/code.js
+++ src/templates/pages/example/modal-search/code.js
@@ -14,7 +14,7 @@ const Code = () => <Fragment>
                 <button class="modal__search-btn">search</button>
             </form>
             <button class="modal__close js-modal-search__btn" aria-label="Close search">
-                <svg focusable="false" aria-hidden="true" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
+                <svg class="modal__close-icon" focusable="false" aria-hidden="true" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
                     <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
                 </svg>
             </button>
