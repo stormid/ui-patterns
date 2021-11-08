07dafe55974d06ea89cea84cd72d1d66b293f8e2
Mick Perkins <mick.perkins@stormid.com>
2021-11-08 16:41:22 +0000

    Update toggle
    Update presentation of modal close

--- src/templates/pages/example/modal-navigation/double/index.js
+++ src/templates/pages/example/modal-navigation/double/index.js
@@ -34,9 +34,7 @@ const ModalNavigation = () => <ExampleLayout>
                             </li>
                         </ul>
                         <button class="modal__close js-modal-nav__toggle" aria-label="Close navigation">
-                            <svg class="modal__close-icon" focusable="false" aria-hidden="true" height="24" viewBox="0 0 24 24" width="24" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z"/>
-                            </svg>
+                            Close
                         </button>
                     </div>
                 </div>
