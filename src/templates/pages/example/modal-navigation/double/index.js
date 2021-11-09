27686472a85dc1e49673f4b2cc4c1dd5c6100f14
Mick Perkins <mick.perkins@stormid.com>
2021-11-09 15:38:43 +0000

    Update button labelling

--- src/templates/pages/example/modal-navigation/double/index.js
+++ src/templates/pages/example/modal-navigation/double/index.js
@@ -1,13 +1,13 @@
 import { h } from 'preact';
 import ExampleLayout from '@layouts/example';
 
-export const title = 'Modal navigation > double code';
+export const title = 'Modal navigation real modal example';
 
 const ModalNavigation = () => <ExampleLayout>
     <header class="modal-nav__container">
         <div class="modal-nav">
             <nav class="modal-nav__modal" aria-label={'Primary navigation'}>
-                <button class="modal-nav__btn js-modal-nav__toggle" aria-label="Show or hide navigation menu" aria-controls="modal--nav" aria-expanded="false">
+                <button class="modal-nav__btn js-modal-nav__toggle" aria-label="Open navigation" aria-controls="modal--nav">
                     <svg focusable="false" class="modal-nav__btn-icon" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
                         <path d="M0 0h24v24H0z" fill="none" />
                         <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" />
--- src/templates/pages/example/modal-navigation/non-modal/index.js
+++ src/templates/pages/example/modal-navigation/non-modal/index.js
@@ -1,13 +1,13 @@
 import { h } from 'preact';
 import ExampleLayout from '@layouts/example';
 
-export const title = 'Modal navigation > non-modal';
+export const title = 'Modal navigation non-modal example';
 
 const ModalNavigation = () => <ExampleLayout>
     <header class="modal-nav__container">
         <div class="modal-nav">
             <nav aria-label={'Primary navigation'}>
-                <button class="modal-nav__btn non-modal-nav__btn js-non-modal-nav__toggle" aria-label="Show or hide navigation menu" aria-controls="modal-fishy-nav" aria-expanded="false">
+                <button class="modal-nav__btn non-modal-nav__btn js-non-modal-nav__toggle" aria-label="Open navigation" aria-controls="modal-fishy-nav" aria-expanded="false">
                     <svg focusable="false" class="modal-nav__btn-icon" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 0 24 24" width="24">
                         <path d="M0 0h24v24H0z" fill="none" />
                         <path d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z" />
