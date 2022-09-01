1addb6262fb87a0f488eaffa294460df31727d95
Mick Perkins <mick.perkins@stormid.com>
2022-09-01 17:31:09 +0100

    Add latest cookie-banner library

--- src/js/modules/cookie-banner/index.js
+++ src/js/modules/cookie-banner/index.js
@@ -1,18 +1,13 @@
-import cookieBanner from './lib';
+import cookieBanner from '@stormid/cookie-banner';
 import toggle from '@stormid/toggle';
 import config from './config';
-const SELECTOR = {
-    TOGGLE: '.js-toggle-banner'
-};
 
 export const init = () => {
     let bannerToggle;
     document.addEventListener('banner.hide', _ => bannerToggle.startToggle()); // clean up className on root element if form in banner was open
-    document.addEventListener('banner.show', _ => [ bannerToggle ] = toggle(SELECTOR.TOGGLE));
+    document.addEventListener('banner.show', _ => [ bannerToggle ] = toggle('.js-toggle-banner'));
 
     const banner = cookieBanner(config);
-    // const retriggerBtn = document.querySelector('.js-preferences-update');
-    // if (retriggerBtn) retriggerBtn.addEventListener('click', e => banner.showBanner());
     [].slice.call(document.querySelectorAll('.js-preferences-update')).forEach(btn => btn.addEventListener('click', e => {
         banner.showBanner();
         bannerToggle.startToggle();
