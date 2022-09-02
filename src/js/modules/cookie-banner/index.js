e8392bf3c196d7b35a224afa652e25faf43a0684
Mick Perkins <mick.perkins@stormid.com>
2022-09-02 15:57:09 +0100

    Add form announcement

--- src/js/modules/cookie-banner/index.js
+++ src/js/modules/cookie-banner/index.js
@@ -1,8 +1,9 @@
-import cookieBanner from '@stormid/cookie-banner';
+import cookieBanner from './lib';
 import toggle from '@stormid/toggle';
 import config from './config';
 
 export const init = () => {
+    if (!document.querySelector('#js-cookie-banner-example')) return;
     let bannerToggle;
     document.addEventListener('banner.hide', _ => bannerToggle.startToggle()); // clean up className on root element if form in banner was open
     document.addEventListener('banner.show', _ => [ bannerToggle ] = toggle('.js-toggle-banner'));
