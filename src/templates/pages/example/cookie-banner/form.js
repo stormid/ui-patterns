0a1c5d0373b8fd3fd3ad74526de2ceb91d27f460
Mick Perkins <mick.perkins@stormid.com>
2022-09-06 16:23:01 +0100

    Update form example page style

--- src/templates/pages/example/cookie-banner/form.js
+++ src/templates/pages/example/cookie-banner/form.js
@@ -6,7 +6,7 @@ export const title = 'Cookie banner example';
 const CookieBanner = () => <ExampleLayout>
     <main class="wrap soft-top" id="js-cookie-banner-example">
         <h1 class="visuallyhidden">Cookie banner form example</h1>
-        <div class="privacy-banner__form-container"></div>
+        <div class="privacy-banner__form-container standalone"></div>
         <div class="privacy-banner__form-announcement" style="position: absolute;width:1px;height:1px;opacity:0;" role="alert"></div>
     </main>
 </ExampleLayout>;
