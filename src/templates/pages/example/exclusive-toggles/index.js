70fa630e4c8109f763d235d26219bbf300f16505
Sarah Richards <sarah.richards@stormid.com>
2023-01-12 09:21:37 +0000

    taking tabindex away from main element

--- src/templates/pages/example/exclusive-toggles/index.js
+++ src/templates/pages/example/exclusive-toggles/index.js
@@ -6,8 +6,9 @@ export const title = 'Exclusive toggles example';
 
 const ExclusiveToggles = () => <ExampleLayout>
     <Code />
-    <main tabindex="0" aria-label="Next focusable item on the page">
-
+    <main class="main">
+        <h1>Lorem ipsum</h1>
+        <p>Duis pulvinar ipsum in lorem maximus faucibus. Maecenas pharetra placerat ex, id elementum felis dignissim sed. <a href="#">Fusce gravida</a> sit amet sapien in interdum. Nam id leo gravida eros gravida elementum. Nulla eu dui posuere, efficitur tellus id, dapibus dolor. Duis tincidunt metus sed condimentum pharetra. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ullamcorper, nibh vel finibus rutrum, urna augue ultricies velit, sed feugiat metus felis in libero.</p>
     </main>
 </ExampleLayout>;
 
