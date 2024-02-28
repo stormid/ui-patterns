8da382a85355f2ec1516eda1130da2c8e766fee5
Sarah Richards <sarah.richards@stormid.com>
2024-02-28 12:47:28 +0000

    Addition of browser list information

--- src/templates/pages/index.js
+++ src/templates/pages/index.js
@@ -1,19 +1,19 @@
-import { h } from 'preact';
-import DefaultLayout from '@layouts/default';
-import { STATUS } from '@constants';
-import Status from '@components/status';
-import { status as cookieBannerStatus } from './patterns/cookie-banner';
-import { status as expandableNavigationStatus } from './patterns/expandable-navigation';
-import { status as fullScreenNavigationStatus } from './patterns/full-screen-navigation';
-import { status as modalSearchStatus } from './patterns/modal-search';
-import { status as expandableSearchStatus } from './patterns/expandable-search';
-import { status as expandableSectionStatus } from './patterns/expandable-section';
-import { status as exclusiveTogglesStatus } from './patterns/exclusive-toggles';
-import { status as modalConfirmationStatus } from './patterns/modal-confirmation';
-import { status as tabsStatus } from './patterns/tabs';
-import { status as formValidationStatus } from './patterns/form-validation';
+import { h } from "preact";
+import DefaultLayout from "@layouts/default";
+import { STATUS } from "@constants";
+import Status from "@components/status";
+import { status as cookieBannerStatus } from "./patterns/cookie-banner";
+import { status as expandableNavigationStatus } from "./patterns/expandable-navigation";
+import { status as fullScreenNavigationStatus } from "./patterns/full-screen-navigation";
+import { status as modalSearchStatus } from "./patterns/modal-search";
+import { status as expandableSearchStatus } from "./patterns/expandable-search";
+import { status as expandableSectionStatus } from "./patterns/expandable-section";
+import { status as exclusiveTogglesStatus } from "./patterns/exclusive-toggles";
+import { status as modalConfirmationStatus } from "./patterns/modal-confirmation";
+import { status as tabsStatus } from "./patterns/tabs";
+import { status as formValidationStatus } from "./patterns/form-validation";
 
-export const title = 'Home';
+export const title = "Home";
 
 // export const meta = [{
 //     name: 'description',
@@ -21,43 +21,55 @@ export const title = 'Home';
 // }];
 
 const PATTERNS = [
-    { title: 'Expandable navigation', url: '/patterns/expandable-navigation', status: expandableNavigationStatus },
-    { title: 'Full screen navigation', url: '/patterns/full-screen-navigation', status: fullScreenNavigationStatus },
-    { title: 'Expandable search', url: '/patterns/expandable-search', status: expandableSearchStatus },
-    { title: 'Modal search', url: '/patterns/modal-search', status: modalSearchStatus },
-    { title: 'Expandable section', url: '/patterns/expandable-section', status: expandableSectionStatus },
-    { title: 'Exclusive toggles', url: '/patterns/exclusive-toggles', status: exclusiveTogglesStatus },
-    { title: 'Modal confirmation', url: '/patterns/modal-confirmation', status: modalConfirmationStatus },
-    { title: 'Cookie banner', url: '/patterns/cookie-banner', status: cookieBannerStatus },
-    { title: 'Tabs', url: '/patterns/tabs', status: tabsStatus },
-    { title: 'Form validation', url: '/patterns/form-validation', status: formValidationStatus },
-    // { title: 'Modal gallery', url: '/patterns/modal-gallery' },
+	{ title: "Expandable navigation", url: "/patterns/expandable-navigation", status: expandableNavigationStatus },
+	{ title: "Full screen navigation", url: "/patterns/full-screen-navigation", status: fullScreenNavigationStatus },
+	{ title: "Expandable search", url: "/patterns/expandable-search", status: expandableSearchStatus },
+	{ title: "Modal search", url: "/patterns/modal-search", status: modalSearchStatus },
+	{ title: "Expandable section", url: "/patterns/expandable-section", status: expandableSectionStatus },
+	{ title: "Exclusive toggles", url: "/patterns/exclusive-toggles", status: exclusiveTogglesStatus },
+	{ title: "Modal confirmation", url: "/patterns/modal-confirmation", status: modalConfirmationStatus },
+	{ title: "Cookie banner", url: "/patterns/cookie-banner", status: cookieBannerStatus },
+	{ title: "Tabs", url: "/patterns/tabs", status: tabsStatus },
+	{ title: "Form validation", url: "/patterns/form-validation", status: formValidationStatus },
+	// { title: 'Modal gallery', url: '/patterns/modal-gallery' },
 ];
 
-
-const HomePage = () => <DefaultLayout>
-    <div class="table__container">
-        <table class="table">
-            <thead>
-                <tr>
-                    <th class="th">Pattern</th>
-                    <th class="th th--status">Status</th>
-                </tr>
-            </thead>
-            <tbody>
-                {
-                    PATTERNS.map(pattern => <tr class="tr">
-                            <td class="td">
-                                <a class="td__link" href={pattern.url}>{pattern.title}</a>
-                            </td>
-                            <td class="td td--status">
-                                <Status type={pattern.status} />
-                            </td>
-                        </tr>)
-                }
-            </tbody>
-        </table>
-    </div>
-</DefaultLayout>;
+const HomePage = () => (
+	<DefaultLayout>
+		<div class="wrap">
+			<div class="table__container push-bottom">
+				<table class="table">
+					<thead>
+						<tr>
+							<th class="th">Pattern</th>
+							<th class="th th--status">Status</th>
+						</tr>
+					</thead>
+					<tbody>
+						{PATTERNS.map((pattern) => (
+							<tr class="tr">
+								<td class="td">
+									<a class="td__link" href={pattern.url}>
+										{pattern.title}
+									</a>
+								</td>
+								<td class="td td--status">
+									<Status type={pattern.status} />
+								</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</div>
+			<p class="push-bottom">
+				These UI patterns have been tested against the{" "}
+				<a href="https://stormid.sharepoint.com/sites/Test/SitePages/Browsers-and-devices.aspx">Storm standard browser list</a>.{" "}
+			</p>
+			<p>
+				An accessibility review was carried out against Windows Desktop NVDA in Chrome and Firefox, VoiceOver on iOS and Android Talkback in December 2022.
+			</p>
+		</div>
+	</DefaultLayout>
+);
 
 export default HomePage;
