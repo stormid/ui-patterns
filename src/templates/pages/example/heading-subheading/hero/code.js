4ac6720a1f6ae6bcfb5996bc33c460906cc69c97
Sarah Richards <sarah.richards@stormid.com>
2024-04-30 13:30:08 +0100

    Writing tests

--- src/templates/pages/example/heading-subheading/hero/code.js
+++ src/templates/pages/example/heading-subheading/hero/code.js
@@ -4,7 +4,7 @@ const Hero = () => <Fragment>
     <section class="hero">
         <hgroup>
             <h1 class="hero__title">Lorem ipsum dolor sit amet</h1>
-            <p class="hero__subtitle" aria-roledescription="subtitle">Mauris consectetur varius ullamcorper. Aliquam erat volutpat.</p>
+            <p class="hero__subtitle">Mauris consectetur varius ullamcorper. Aliquam erat volutpat.</p>
         </hgroup>
         <div class="hero__body push-top">
             <a href="#" class="btn" aria-label="Read more about Lorem ipsum dolor sit amet">Read more</a>
--- src/templates/pages/patterns/heading-subheading/index.js
+++ src/templates/pages/patterns/heading-subheading/index.js
@@ -17,16 +17,16 @@ const ExpandableSearch = () => <PatternLayout>
     <h2 class="push-bottom--half plus-2 medium">Guidance</h2>
     <p class="push-bottom">Often there is a requirement for a page or a component to contain a subheading or a tagline immediately below the main header of the element.  Visually this may look like a secondary heading such as an h2 or h3, but semantically this text is rarely ever useful as a heading for sectioning content</p>
     <p class="push-bottom">This content should be rendered as standard page paragraphs, but with additional markup to indicate to browsers and screenreaders what the meaning of this content is and how it connects to the heading.</p>
-    <p class="push-bottom--double">At the time of writing, support of the <code class="pre--inline">role=&quot;doc-subtitle&quot;</code> attribute is patchy and implementations contain bugs that make it detrimental to the legibility of the page on some screenreaders (See references below).  For now, the recommendation is to use the <code class="pre--inline">&lt;hgroup&gt;</code> tag along with an <code class="pre--inline">aria-roledescription</code> as a progressive enhancement.</p>
+    <p class="push-bottom--double">At the time of writing, support of the <code class="pre--inline">role=&quot;doc-subtitle&quot;</code> attribute is patchy and implementations contain bugs that make it detrimental to the legibility of the page on some screenreaders (See references below).  For now, the recommendation is to use the <code class="pre--inline">&lt;hgroup&gt;</code> tag alone.</p>
 
     
     <h2 class="push-bottom--half plus-2 medium">Example page hero</h2>
-    <iframe style="--height: 375px" class="example" title="Example peage header with subheading" src={'/example/heading-subheading/hero'}></iframe>
+    <iframe style="--height: 375px" class="example" title="Example page header with subheading" src={'/example/heading-subheading/hero'}></iframe>
     <p class="push-bottom align-right"><a href="/example/heading-subheading/hero" rel="noopener" target="_blank">Open in a new tab</a></p>
     <pre class="pre"><code class="code">{`${render(<CodeHero />, null, { pretty: true })}`}</code></pre>
 
     <h2 class="push-bottom--half plus-2 medium">Example card</h2>
-    <iframe style="--height: 375px" class="example" title="Example peage header with subheading" src={'/example/heading-subheading/card'}></iframe>
+    <iframe style="--height: 375px" class="example" title="Example card header with subheading" src={'/example/heading-subheading/card'}></iframe>
     <p class="push-bottom align-right"><a href="/example/heading-subheading/card" rel="noopener" target="_blank">Open in a new tab</a></p>
     <pre class="pre"><code class="code">{`${render(<CodeCard />, null, { pretty: true })}`}</code></pre>
 
@@ -37,7 +37,7 @@ const ExpandableSearch = () => <PatternLayout>
     <h3 class="push-bottom--half plus-1 medium">For validation in developer tools / web inspector</h3>
     <ul class="list list--tick push-bottom--double">
         <li class="list-item">Any headings followed by a tagline or immediate subtitle should be wrapped in an <pre class="pre--inline">&lt;hgroup&gt;</pre> tag</li>
-        <li class="list-item">Any taglines or subtitles should be <pre class="pre--inline">&lt;p&gt;</pre> tag with an <pre class="pre--inline">aria-roledescription="subtitle"</pre> attribute</li>
+        <li class="list-item">Any taglines or subtitles should be <pre class="pre--inline">&lt;p&gt;</pre> tag</li>
         <li class="list-item">The level of the heading within the <pre class="pre--inline">&lt;hgroup&gt;</pre> tag will depend on the surrounding page context, and should follow the page heirarchy</li>
         <li class="list-item">The <pre class="pre--inline">&lt;hgroup&gt;</pre> must follow HTML5 validation rules and contain only one heading tag with one or more supporting paragraphs</li>
     </ul>
@@ -49,8 +49,6 @@ const ExpandableSearch = () => <PatternLayout>
 
     <h3 class="push-bottom--half plus-1 medium">For functional validation</h3>
     <ul class="list list--tick push-bottom--double">
-        <li class="list-item">Those screenreaders which support <pre class="pre--inline">aria-roledescription</pre> should announce "Subtitle" while reading the tagline or subtitle text.</li>
-        <li class="list-item">Those screenreaders which do not support <pre class="pre--inline">aria-roledescription</pre> should announce the text only, with no detrimental effect</li>
         <li class="list-item">The subtitle should not appear as a heading within the accessibility tree, and should not be announced as such</li>
     </ul>
 
