bb379938d4f24101e8c2f6b8dde93dd36dfd95e1
Sarah Richards <sarah.richards@stormid.com>
2024-08-19 14:31:04 +0100

    Date picker - finish conversion of existing tests

--- __tests__/playwright/patterns/date-picker/index.spec.js
+++ __tests__/playwright/patterns/date-picker/index.spec.js
@@ -2,14 +2,59 @@ const { test, expect } = require("@playwright/test");
 import { reducedProjects } from 'playwright.config';
 import AxeBuilder from '@axe-core/playwright';
 
+const daysOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const zeropad = num => num.toString().padStart(2, '0');
+
 test.beforeEach(async ({ page }) => {
 	await page.goto("/example/date-picker/");
 });
 
-// test.describe("Date picker > Functionality", () => {
-	
+test.describe("Date picker single input > Functionality", () => {
+	test('Should not allow a user to interact with the calendar until opened', async ({ page }) => {
+		const dialog = page.locator(".ds_datepicker__dialog");
+        await expect(dialog).not.toBeVisible();
 
-// });
+		const calendarButton = page.locator(".js-calendar-button");
+		await calendarButton.click();
+		await expect(dialog).toBeVisible();
+
+		await calendarButton.click();
+		await expect(dialog).not.toBeVisible();
+    });
+
+	test('Should focus on todays date when opened', async ({ page }) => {
+		const calendarButton  = page.locator('.js-calendar-button');
+        await calendarButton.click();
+        
+		const focusedElement = page.locator(':focus');
+		await expect(focusedElement).toHaveRole("button");
+		await expect(focusedElement).toHaveClass(/ds_datepicker__today/);
+        await calendarButton.click();
+    });
+
+	test('Should populate input value with selected date and update accessible text of button', async ({ page }) => {
+		const today = new Date();
+        const day = today.getDate();
+        const month = today.getMonth();
+        const year = today.getFullYear();
+		const dayOfWeek = today.getDay();
+		let textContentString = new RegExp(String.raw`Choose date. Selected date is ${daysOfWeek[dayOfWeek]} ${day} ${months[month]} ${year}`, "g");
+
+		const dateInput = page.locator('input');
+		const calendarButton  = page.locator('.js-calendar-button');
+		const todayButton = page.locator('.ds_datepicker__today');
+
+		await dateInput.fill("");
+
+		await todayButton.click();
+		await expect(calendarButton).toContainText(textContentString);
+		await expect(dateInput).toHaveValue(`${zeropad(day)}/${zeropad(month+1)}/${year}`);
+		await calendarButton.click();
+        
+    });
+
+});
 
 // test.describe("Date picker > Keyboard", () => {
 	
@@ -28,7 +73,7 @@ test.describe("Date picker single input > Markup tests", () => {
 	});
 
 	test('Should use a button element for the calendar trigger', async ({ page }) => {
-		const button = await page.locator(".js-calendar-button");
+		const button = page.locator(".js-calendar-button");
         await expect(button).toHaveRole("button");
     });
 	
@@ -48,6 +93,33 @@ test.describe("Date picker single input > Aria", () => {
 			const label = await day.getAttribute('aria-label')
             expect(label).not.toBeNull();
         }
+    });
+
+	test('Should toggle the calendar button as aria-expanded when opened and closed', async ({ page }) => {
+		const calendarButton  = page.locator('.js-calendar-button');
+		const todayButton = page.locator('.ds_datepicker__today');
+
+        await calendarButton.click();
+		expect(await calendarButton.getAttribute('aria-expanded')).toEqual('true');
+
+        await todayButton.click();
+		expect(await calendarButton.getAttribute('aria-expanded')).toEqual('false');
+    });
+
+	test('Should have an accurate aria label for date buttons', async ({ page }) => {
+		const today = new Date();
+        const dayOfWeek = today.getDay();
+        const day = today.getDate();
+        const month = today.getMonth();
+        const year = today.getFullYear();
+
+		const calendarButton  = page.locator('.js-calendar-button');
+        await calendarButton.click();
+        
+		const focusedElement = page.locator(':focus');
+		const focusedAria = await focusedElement.getAttribute('aria-label')
+        expect(focusedAria).toEqual(`${daysOfWeek[dayOfWeek]} ${day} ${months[month]} ${year}`);
+        await calendarButton.click();
     });
 });
 
